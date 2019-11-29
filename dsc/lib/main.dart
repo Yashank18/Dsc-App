33421703a6991388111db54f44a07879d8956641
Yashank18 <yashank1050.cse18@chitkara.edu.in>
2019-11-29 23:55:45 +0530

    image and container added

--- dsc/lib/main.dart
+++ dsc/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,9 +9,47 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     
     return MaterialApp(
-      
-      
-    );
+        home: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.white,
+            
+            title: Text("Developer Student Clubs",style: TextStyle(color: Colors.black),),
+            actions: <Widget>[
+              Icon(Icons.supervised_user_circle,color: Colors.black,size: 36.0,),
+            ],
+          ),
+          //Curved Container
+          body: newapp(),
+         
+            
+            
+            
+          ),
+           
+        );
+        
+    
   }
 }
+class newapp extends StatelessWidget{
 
+@override
+  Widget build(BuildContext context) {
+
+      return ListView(
+        children: <Widget>[
+          Image.asset('assets/dsc.png'),
+          Container(
+            height: 400,
+            decoration: BoxDecoration(
+              color: Colors.lightBlueAccent,
+              borderRadius: BorderRadius.only(topLeft: Radius.circular(95.0),topRight: Radius.circular(95.0))
+            ),
+          
+          )
+        ],
+      );
+  
+
+}
+}
