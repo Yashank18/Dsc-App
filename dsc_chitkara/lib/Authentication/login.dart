e26a29a70a468a7caba238fe34b3003488ebf71f
Yashank18 <yashank1050.cse18@chitkara.edu.in>
2019-12-22 23:10:21 +0530

    Login Page UI Done (Without Image)

--- dsc_chitkara/lib/Authentication/login.dart
+++ dsc_chitkara/lib/Authentication/login.dart
@@ -1,36 +1,74 @@
 import 'package:flutter/material.dart';
 import 'dart:ui';
 
-
+import 'package:flutter/material.dart' as prefix0;
 
 class LoginPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
+   
+  const LoginPage({Key key}) : super(key: key);
+
+  Widget _buildPageContent(){
+    
     return Container(
-      color: Colors.blue.shade100,
+      padding: EdgeInsets.all(20.0),
+      color: Colors.grey.shade500,
       child: ListView(
-      children: <Widget>[
-        SizedBox(height: 30.0,),
-        //have to add image here
-        CircleAvatar(child:Image.asset("assets/stars.jpg"),maxRadius: 50,backgroundColor: Colors.transparent,),
-        SizedBox(height: 20.0,),
-        _buildLoginForm(),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            FlatButton(
-              onPressed: (){},
-              child: Text("Sign Up",style:TextStyle(color: Colors.blue,fontSize: 18.0),),
-            )
-          ],
-        )
-      ],
-      )
+        children: <Widget>[
+          Column(
+            children: <Widget>[
+              SizedBox(height: 50,),
+              //have to add correct image source
+              Container(width: 200,child: prefix0.Image.asset("assets/star.jpg"),),
+              SizedBox(height: 50,),
+              ListTile(
+                title: TextField(
+                  style: TextStyle(color: Colors.white),
+                  decoration: InputDecoration(
+                    hintText: "Email Adress",
+                    hintStyle: prefix0.TextStyle(color: Colors.white70),
+                    border: InputBorder.none,
+                    icon: Icon(Icons.email,color: Colors.white30,)
+                  ),
+                ),
+              ),
+              Divider(color: Colors.grey.shade600,),
+              ListTile(
+                title: TextField(
+                  obscureText: true,
+                  style: TextStyle(color: Colors.white),
+                  decoration: InputDecoration(
+                    hintText: "Password",
+                    hintStyle: prefix0.TextStyle(color: Colors.white70),
+                    border: InputBorder.none,
+                    icon: Icon(Icons.lock,color: Colors.white30,)
+                  ),
+                ),
+              ),
+              Divider(color: Colors.grey.shade600,),
+              SizedBox(height: 20,),
+              Row(
+                children: <Widget>[
+                  Expanded(
+                    child: RaisedButton(
+                      onPressed: (){},
+                      color: Colors.cyan,
+                      child: Text("Logn",style: prefix0.TextStyle(color: Colors.white70),),
+                    ),
+                  )
+                ],
+              ),
+              SizedBox(height: 40,),
+              Text("Forget your Password?",style: TextStyle(color: Colors.grey.shade500),)
+            ],
+          )
+        ],
+      ),
     );
   }
-}
-// main form data
-Container _buildLoginForm()
-{
-  return Container();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _buildPageContent(),
+    );
+  }
 }
