b608ad4982a1aa9a2c46175648094f022a2314e1
Yashank18 <yashank7696@gmail.com>
2020-03-28 23:42:32 +0530

    New Start

--- dsc_chitkara/lib/main.dart
+++ dsc_chitkara/lib/main.dart
@@ -1,50 +1,99 @@
-import 'package:dsc_chitkara/SplashScreen.dart';
 import 'package:flutter/material.dart';
-import "Authentication/AuthPage.dart";
-import 'Authentication/login.dart';
-import 'dart:ui';
-
-import 'Authentication/signup.dart';
-
-import 'home.dart';
 
 void main() => runApp(MyApp());
 
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-
-Map<int,Color> color={
-  50:Color.fromRGBO(195, 230, 228, .1),
-  100:Color.fromRGBO(195, 230, 228, .2),
-  200:Color.fromRGBO(195, 230, 228, .3),
-  300:Color.fromRGBO(195, 230, 228, .4),
-  400:Color.fromRGBO(195, 230, 228, .5),
-  500:Color.fromRGBO(195, 230, 228, .6),
-  600:Color.fromRGBO(195, 230, 228, .7),
-  700:Color.fromRGBO(195, 230, 228, .8),
-  800:Color.fromRGBO(195, 230, 228, .9),
-  900:Color.fromRGBO(195, 230, 228, 1),
-
-};
- 
+  const MyApp({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-     MaterialColor colorCustom =MaterialColor(0xFFC3E6E4, color);
     return MaterialApp(
-      home: SplashScreen(),
-      theme: new ThemeData(
-        primarySwatch: colorCustom,
-      ),
-      debugShowCheckedModeBanner: false,
-      routes: <String, WidgetBuilder>{
-        '/auth':(BuildContext context)=>Auth(),
-        '/home':(BuildContext context)=>MyHomePage(),
-      }
+     debugShowCheckedModeBanner: false,
+      title: "Developer Student Clubs, Chitkara University",
+      home: MyHome(),
     );
   }
 }
 
+class MyHome extends StatefulWidget {
+  MyHome({Key key}) : super(key: key);
 
+  @override
+  _MyHomeState createState() => _MyHomeState();
+}
 
+class _MyHomeState extends State<MyHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+      body: Container(
+        color: Color.fromRGBO(39, 39, 47, 1),
+       child: Center(
+         child:Column(
+           mainAxisAlignment: MainAxisAlignment.center,
+           children: <Widget>[
+             Container(child: Image.asset("images/dsc_trans.png",width: 150.0,),
+             color: Color.fromRGBO(39, 39, 47, 1),
+             ),
+             SizedBox(height:70.0),
+             Center(child: Text("Welcome to DSC Chitkara",style: TextStyle(color:Color.fromRGBO(55, 239, 187, 1),fontSize: 25.0,fontWeight: FontWeight.bold),),),
+             SizedBox(height:20),
+             Column(
+               children: <Widget>[
+                  
+                     Card(
+                       color: Colors.black,
+                       elevation: 8.0,
+                      margin: EdgeInsets.symmetric(horizontal:10.0,vertical:6.0),
+                        child: ListTile(
+                          contentPadding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+                          leading: Container(
+                                decoration: new BoxDecoration(
+                                    border: new Border(
+                                        right: new BorderSide(width: 1.0, color: Colors.white24))),
+                                child: Icon(Icons.mood, color: Colors.white),
+
+                              ),
+                              title: Text(
+                              "Number Of people Recovered",
+                              style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+                            ),
+                            subtitle: Row(
+                                    children: <Widget>[
+                                      Icon(Icons.linear_scale, color: Colors.green),
+                                      Text("  Me",style: TextStyle(color:Colors.green),),
+                                    ],
+                                  ),
+                        ),
+                     ),
+                   
+               ],
+             ),
+             /*Card(
+               shape: BeveledRectangleBorder(borderRadius:BorderRadius.circular(5.0)),
+               color: Color.fromRGBO(55, 55, 63, 1),
+               margin: EdgeInsets.only(top:30,bottom: 80,left:20,right:20),
+               child:Column(
+                 children: <Widget>[
+                   
+                   Row(
+                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                     children: <Widget>[
+                       Text("left is the big one here"),
+                       Text("Right"),
+                         
+                     ],
+                   ),
+
+                 ],
+               ),
+             ),*/
+           ],
+         )
+       )
+      ),
+    );
+  }
+}
