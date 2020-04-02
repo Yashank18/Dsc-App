69f6064be3723ad10f1bd3a5c9d9c15257a0e0f0
Yashank18 <yashank7696@gmail.com>
2020-04-03 01:09:04 +0530

    FirstBuiltReady

--- dsc_chitkara/lib/aboutUs.dart
+++ dsc_chitkara/lib/aboutUs.dart
@@ -29,7 +29,7 @@ class _AboutUsState extends State<AboutUs> {
           children: <Widget>[
             Image.asset("images/dsc.png"),
             SizedBox(height: 30,),
-             Text("What is DSC ?",style: GoogleFonts.varelaRound(textStyle:TextStyle(color: Colors.black,fontSize: 40,fontWeight: FontWeight.w800),) ),
+             Padding(padding:EdgeInsets.only(left:10),child: Text("What is DSC ?",style: GoogleFonts.varelaRound(textStyle:TextStyle(color: Colors.black,fontSize: 40,fontWeight: FontWeight.w800),) )),
             
             SizedBox(height: 30,),
              Card(elevation: 0.0,child: Padding(padding:EdgeInsets.all(15),child: Text("Developer Student Clubs are university based community groups for students interested in Google developer technologies. Students from all undergraduate or graduate programs with an interest in growing as a developer are welcome. By joining a DSC, students grow their knowledge in a peer-to-peer learning environment and build solutions for local businesses and their community. Developer Student Clubs, Chitkara University Mainly focuses on Overall development of Society",style: GoogleFonts.varelaRound(textStyle:TextStyle(height: 1.5)),)),)
